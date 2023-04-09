9c0d452e57c3bd7315a348bc91f4f3ee1cb8bc0c
My Name <cpgsom819@gmail.com>
2023-04-09 13:09:00 +0800

    displayed the data

--- script.js
+++ script.js
@@ -1,19 +1,29 @@
 
 
 const loadAllProducts =async () => {
-
     const response = await fetch('https://fakestoreapi.com/products');
     const data = await response.json();
     return data;
-
 }
 
 
-const setAllMenu = async() => {
-
+ const setAllMenu = async() => {     
     const data = await loadAllProducts();
+    const menu = document.getElementById('all-menu');
+    const uniqueArray = [];
+    for(const product of data) {
 
-    console.log(data);
+
+            if(uniqueArray.indexOf(product.category)=== -1){
+            uniqueArray.push(product.category);
+            const li = document.createElement('li');
+            li.innerHTML = `<a>${product.category}</a>`;
+            menu.appendChild(li);
+
+        }
+
+
+      }
 
 
 }
